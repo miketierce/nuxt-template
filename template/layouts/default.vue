c1495848f24cafc4733ee57a289e43505281e893
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-08-18 22:55:38 +0700

    Update default.vue

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -1,53 +1,19 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <section>
+    <b-navbar toggleable type="light" variant="light">
+      <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
+      <b-container>
+        <b-navbar-brand>{{ name }}</b-navbar-brand>
+        <b-collapse is-nav id="nav_text_collapse">
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <b-container>
+      <nuxt />
+    </b-container>
+  </section>
 </template>
-
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
-
