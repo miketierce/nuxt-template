671d532dbde0b56ce69b0e996c92f513f8ba051a
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-08-18 21:42:29 +0700

    Migrate CSS from index.vue to style.scss

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -24,11 +24,11 @@
 
 <script>
 import axios from 'axios'
-import AppLogo from '~/components/AppLogo.vue'
+import logo from '~/components/AppLogo.vue'
 
 export default {
   components: {
-    AppLogo
+    'app-logo': logo
   },
   data () {
     return {
@@ -42,35 +42,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 5rem;
-  color: #35495e;
-  letter-spacing: 1px;
-  margin-top: 1rem;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
